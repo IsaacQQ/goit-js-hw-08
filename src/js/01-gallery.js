0b74c3bdbbf4b099fbe261bd0abc9248cf1463bf
Игорь <bujhtyz228@gmail.com>
2022-10-02 21:04:08 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,24 +8,45 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector('.gallery');
-const items = [];
+const galleryDiv = document.querySelector('.gallery')
+const store ={lightbox : null}
+const markup = galleryItems.reduce(
+  (acc, {preview, original, description}) =>
+  acc +
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`,
+    ""
+);
 
-galleryItems.forEach(element => {
-  const galleryLink = document.createElement('a');
-  galleryLink.className = 'gallery__link';
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement('img');
-  galleryImage.className = 'gallery__image';
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute('title', element.description);
-  galleryImage.alt = element.description;
+galleryDiv.insertAdjacentHTML("beforeend", markup);
 
-  galleryLink.append(galleryImage);
-  items.push(galleryLink);
+galleryDiv.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt){
+  console.log(evt)
+  evt.preventDefault();
+  const galleryItem = evt.target.closest('.gallery__item');
+  if(!galleryItem) return;
+  
+  const img = galleryItem.querySelector('.gallery__image')
+  const src = img.dataset.source;
+  showModal(src);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+
 });
-gallery.append(...items);
 
-new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
+
+
+
